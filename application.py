6bf8de1f74d1e039ad763125480131f63302d325
jerryespi13 <jerryespi@gmail.com>
2023-09-24 10:54:39 -0600

    añadiendo dotenv para trabajar con archivo .env

--- application.py
+++ application.py
@@ -3,6 +3,12 @@ import os
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
+# para trabajar con variables de entorno desde el archivo .
+from dotenv import load_dotenv
+
+# carga las variables de entorno desde el archivo .env
+load_dotenv()
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
