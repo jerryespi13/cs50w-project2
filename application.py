00a820dc0806ceb2c9c53276febbb2cf3bcdb822
jerryespi13 <jerryespi@gmail.com>
2023-09-26 17:05:32 -0600

    obtencion de usuario y validacion

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash
 from flask_socketio import SocketIO, emit
 
 # para trabajar con variables de entorno desde el archivo .
@@ -10,12 +10,29 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 socketio = SocketIO(app, cors_allowed_origin="*")
 
+# variable donde se guardan los usuarios
+usuarios=list()
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    print(usuarios)
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        usuario = request.form.get("usuario")
+        print(usuario)
+        if usuario == "" or usuario == " ":
+            flash("Usuario invalido")
+            return redirect("/")
+        if usuario in usuarios:
+            flash('Usuario ya existe')
+            return redirect("/")
+        else:
+            usuarios.append(usuario)
+            return redirect("/canales")
 
 @app.route('/canales')
 def canales():
